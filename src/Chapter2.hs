db99c07d3c9d6d028e81dc071be8563325315e49
Barry Moore <chiroptical@gmail.com>
2025-07-11 16:48:56 -0400

    Finish Exercise 2.3

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -1,7 +1,17 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Chapter2 where
 
 import Control.Monad.Except
 import Control.Monad.Log
+import Control.Monad.Random
+import Control.Monad.Reader
+import Control.Monad.State
+import Data.Foldable
+import Data.Map (Map)
+import Data.Map qualified as Map
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Prettyprinter
@@ -86,6 +96,164 @@ parseLVar = \case
   ast -> do
     logSnailAst "Invalid LVar expression" ast
     throwError InvalidLVarExpression
+
+newtype RenameMap = RenameMap {renameMap :: Map Text Text}
+
+-- | Returns random characters in the alphabet either upper or lower case
+randomChar :: (MonadRandom m) => m Char
+randomChar = uniform $ ['a' .. 'z'] <> ['A' .. 'Z']
+
+-- | Generate a 10 digit string of random characters
+uniqueName :: (MonadRandom m) => m Text
+uniqueName = Text.pack <$> replicateM 10 randomChar
+
+-- | Exercise 2.1 (pg 26):  Make all variable names unique
+uniquifyLExpr :: (RandomGen g, MonadReader RenameMap m) => LExpr -> RandT g m LExpr
+uniquifyLExpr = \case
+  -- Nothing to do with literals
+  x@(LInt _) -> pure x
+  x@LRead -> pure x
+  -- recursive cases, but no uniqueness to deal with
+  LPlus x y -> LPlus <$> uniquifyLExpr x <*> uniquifyLExpr y
+  LUnaryMinus x -> LUnaryMinus <$> uniquifyLExpr x
+  LMinus x y -> LMinus <$> uniquifyLExpr x <*> uniquifyLExpr y
+  -- recursive cases, uniqueness matters
+  LVar x -> do
+    RenameMap {renameMap} <- ask
+    -- we only need to generate a new name, if it doesn't already have a binding
+    case Map.lookup x renameMap of
+      Nothing ->
+        LVar <$> uniqueName
+      Just existingBinding ->
+        pure $ LVar existingBinding
+  LLet x expr body -> do
+    uniqueX <- uniqueName
+    uniqueExpr <- uniquifyLExpr expr -- can't use variable in definition
+    uniqueBody <- local (\RenameMap {renameMap} -> RenameMap {renameMap = Map.insert x uniqueX renameMap}) $ uniquifyLExpr body
+    pure $ LLet uniqueX uniqueExpr uniqueBody
+
+uniquifyLVar :: (RandomGen g, MonadReader RenameMap m) => LVar -> RandT g m LVar
+uniquifyLVar = \case
+  LProgram () lexpr -> LProgram () <$> uniquifyLExpr lexpr
+
+-- | 'LInt', 'LRead', 'LVar' are all simple atomic expressions
+isSimpleAtomicExpression :: LExpr -> Bool
+isSimpleAtomicExpression = \case
+  LInt {} -> True
+  LRead -> True
+  LVar {} -> True
+  LUnaryMinus {} -> False
+  LPlus {} -> False
+  LMinus {} -> False
+  LLet {} -> False
+
+{- | An expression is atomic when each component is atomic. 'LInt', 'Read',
+'LVar' are all atomic expressions. 'LUnaryMinus' is atomic when it's argument
+is atomic.
+-}
+isAtomic :: LExpr -> Bool
+isAtomic = \case
+  LInt {} -> True
+  LRead -> True
+  LVar {} -> True
+  LUnaryMinus x -> isSimpleAtomicExpression x
+  LPlus x y -> isSimpleAtomicExpression x && isSimpleAtomicExpression y
+  LMinus x y -> isSimpleAtomicExpression x && isSimpleAtomicExpression y
+  LLet _x expr body ->
+    -- If the expression bound to the variable is atomic and the entire body is
+    -- built of atomic expressions, the whole expression is atomic
+    isSimpleAtomicExpression expr && isAtomic body
+
+{- | If the 'LExpr' is a simple atomic expression, return it, otherwise you need
+to make it atomic
+-}
+single ::
+  (MonadLog (WithSeverity (Doc ann)) m, MonadState [(Text, LExpr)] m, RandomGen g) =>
+  LExpr ->
+  RandT g m LExpr
+single x = do
+  if isSimpleAtomicExpression x
+    then pure x
+    else do
+      name <- uniqueName
+      newX <- makeAtomic x
+      modify $ \s -> [(name, newX)] <> s
+      pure $ LVar name
+
+lexprToText :: LExpr -> Text
+lexprToText = \case
+  LInt int -> Text.pack $ show int
+  LRead -> "read"
+  LUnaryMinus lexpr -> "(- " <> lexprToText lexpr <> ")"
+  LPlus x y -> "(+ " <> lexprToText x <> " " <> lexprToText y <> ")"
+  LMinus x y -> "(- " <> lexprToText x <> " " <> lexprToText y <> ")"
+  LVar var -> "\"" <> var <> "\""
+  LLet var x y -> "(let (" <> var <> lexprToText x <> ")" <> lexprToText y <> ")"
+
+logLExpr :: forall ann m. (MonadLog (WithSeverity (Doc ann)) m) => Text -> LExpr -> m ()
+logLExpr msg lexpr = logInfo . pretty $ msg <> ": " <> lexprToText lexpr
+
+makeAtomic ::
+  forall m g ann.
+  (MonadLog (WithSeverity (Doc ann)) m, MonadState [(Text, LExpr)] m, RandomGen g) =>
+  LExpr ->
+  RandT g m LExpr
+makeAtomic = \case
+  -- No state changes needed
+  x@(LInt _) -> pure x
+  x@LRead -> pure x
+  x@(LVar _) -> pure x
+  -- May require state changes if not atomic
+  expr@(LPlus x y) ->
+    if isAtomic expr
+      then pure expr
+      else do
+        newX <- single x
+        newY <- single y
+        pure $ LPlus newX newY
+  expr@(LUnaryMinus x) ->
+    if isAtomic expr
+      then pure expr
+      else do
+        newX <- single x
+        pure $ LUnaryMinus newX
+  LMinus x y -> do
+    newX <- single x
+    newY <- single y
+    pure $ LMinus newX newY
+  lexpr@(LLet x expr body) -> do
+    if isAtomic lexpr
+      then do
+        lift $ logLExpr "is atomic: " lexpr
+        pure lexpr
+      else do
+        lift $ logLExpr "is not atomic: " lexpr
+        newExpr <- single expr
+        newBody <- single body
+        pure $ LLet x newExpr newBody
+
+buildLExprFromNonComplexOperands :: (LExpr, [(Text, LExpr)]) -> LExpr
+buildLExprFromNonComplexOperands (ast, definitions) =
+  let f :: LExpr -> (Text, LExpr) -> LExpr
+      f nonComplex (name, expr) = LLet name expr nonComplex
+   in foldl' f ast definitions
+
+{- | Exercise 2.3 (pg 28)
+
+This function forces 'LPlus' or 'LMinus's to act only on 'LInt' or 'LVar'
+
+(let (x (+ 42 (- 10))) (+ x 10))
+        ^^^^^^^^^^^^^
+This is not allowed because (- 10) is an operation.
+It needs to be (let (tmp (- 10)) (+ 42 tmp))
+-}
+removeComplexOperands ::
+  (MonadLog (WithSeverity (Doc ann)) m) => LVar -> m LVar
+removeComplexOperands (LProgram () lexpr) = do
+  let st = flip runStateT []
+      program = st $ evalRandT (makeAtomic lexpr) $ mkStdGen 2023
+  results :: (LExpr, [(Text, LExpr)]) <- program
+  pure $ LProgram () $ buildLExprFromNonComplexOperands results
 
 -- C_var
 data CAtom
--- test/Chapter2Spec.hs
+++ test/Chapter2Spec.hs
@@ -1,6 +1,7 @@
 module Chapter2Spec (spec) where
 
 import Chapter2
+import Control.Monad.Random
 import Data.Text
 import RunM
 import Snail
@@ -31,6 +32,162 @@ spec = do
       ast
         `shouldBe` Left InvalidLVarExpression
 
+  describe "Exercise 2.2" do
+    it "handles singly nested let" do
+      lexprE <- snailToLExpr "(let (x 10) x)"
+      lexprE `shouldBe` Right (LLet "x" (LInt 10) (LVar "x"))
+      let lexpr = unsafeFromRight lexprE
+      (x, y) <-
+        runUniquify lexpr >>= \case
+          LLet x (LInt 10) (LVar y) -> pure (x, y)
+          _ -> assertFailure "Unable to parse resulting LVar"
+      x `shouldNotBe` "x"
+      x `shouldBe` y
+    it "handles doubly nested let" do
+      lexprE <- snailToLExpr "(let (x 10) (+ (let (x 10) x) x))"
+      lexprE `shouldBe` Right (LLet "x" (LInt 10) (LPlus (LLet "x" (LInt 10) (LVar "x")) (LVar "x")))
+      let lexpr = unsafeFromRight lexprE
+      (a, b, c, d) <-
+        runUniquify lexpr >>= \case
+          LLet a (LInt 10) (LPlus (LLet b (LInt 10) (LVar c)) (LVar d)) -> pure (a, b, c, d)
+          _ -> assertFailure "Unable to parse resulting AST"
+      -- The variables should be new names
+      a `shouldNotBe` "x"
+      b `shouldNotBe` "x"
+      -- The variables should be named different from one-another
+      a `shouldNotBe` b
+      -- The outer variables should match
+      a `shouldBe` d
+      -- The inner variables should match
+      b `shouldBe` c
+    it "handles let nested in body" do
+      lexprE <- snailToLExpr "(let (x 10) (let (x 5) (+ x x)))"
+      lexprE `shouldBe` Right (LLet "x" (LInt 10) (LLet "x" (LInt 5) (LPlus (LVar "x") (LVar "x"))))
+      let lexpr = unsafeFromRight lexprE
+      (a, b, c, d) <-
+        runUniquify lexpr >>= \case
+          LLet a (LInt 10) (LLet b (LInt 5) (LPlus (LVar c) (LVar d))) -> pure (a, b, c, d)
+          _ -> assertFailure "Unable to parse resulting AST"
+      -- The variables should have new names
+      a `shouldNotBe` "x"
+      b `shouldNotBe` "x"
+      -- They should be named differently
+      a `shouldNotBe` b
+      -- The latter variable is used in the body
+      b `shouldBe` c
+      b `shouldBe` d
+    it "handles reassigned variable in body" do
+      lexprE <- snailToLExpr "(let (x 10) (let (y x) (+ y y))) "
+      lexprE `shouldBe` Right (LLet "x" (LInt 10) (LLet "y" (LVar "x") (LPlus (LVar "y") (LVar "y"))))
+      let lexpr = unsafeFromRight lexprE
+      (a, b, c, d, e) <-
+        runUniquify lexpr >>= \case
+          LLet a (LInt 10) (LLet b (LVar c) (LPlus (LVar d) (LVar e))) -> pure (a, b, c, d, e)
+          _ -> assertFailure "Unable to parse resulting AST"
+      -- The variables should have new names
+      a `shouldNotBe` "x"
+      b `shouldNotBe` "y"
+      -- They should be named differently
+      a `shouldNotBe` b
+      -- The variables match their bodies
+      a `shouldBe` c
+      b `shouldBe` d
+      b `shouldBe` e
+    it "handles nested let variable in variable definition" do
+      lexprE <- snailToLExpr "(let (x (let (x 10) x)) x)"
+      lexprE `shouldBe` Right (LLet "x" (LLet "x" (LInt 10) (LVar "x")) (LVar "x"))
+      let lexpr = unsafeFromRight lexprE
+      (a, b, c, d) <-
+        runUniquify lexpr >>= \case
+          LLet a (LLet b (LInt 10) (LVar c)) (LVar d) -> pure (a, b, c, d)
+          _ -> assertFailure "Unable to parse resulting AST"
+      -- The variables should have new names
+      a `shouldNotBe` "x"
+      b `shouldNotBe` "x"
+      -- They should be named differently
+      b `shouldNotBe` a
+      -- They should match their bodies
+      a `shouldBe` d
+      b `shouldBe` c
+    it "handles unused let variable in variable definition" do
+      lexprE <- snailToLExpr "(let (x (let (y 10) x)) x)"
+      lexprE `shouldBe` Right (LLet "x" (LLet "y" (LInt 10) (LVar "x")) (LVar "x"))
+      let lexpr = unsafeFromRight lexprE
+      (a, b, c, d) <-
+        runUniquify lexpr >>= \case
+          LLet a (LLet b (LInt 10) (LVar c)) (LVar d) -> pure (a, b, c, d)
+          _ -> assertFailure "Unable to parse resulting AST"
+      -- The variables should have new names
+      a `shouldNotBe` "x"
+      b `shouldNotBe` "y"
+      c `shouldNotBe` "x"
+      -- The x in the definition should be different
+      c `shouldNotBe` a
+      -- The correct variable used in the body
+      a `shouldBe` d
+
+    describe "removeComplexOperands" do
+      it "handles makeAtomic with simple input" do
+        lexprE <- snailToLExpr "(+ 42 (- 10))"
+        lexprE `shouldBe` Right (LPlus (LInt 42) (LUnaryMinus (LInt 10)))
+        let lexpr = unsafeFromRight lexprE
+        LProgram () nonComplexAst <- runLogM $ removeComplexOperands $ LProgram () lexpr
+        case nonComplexAst of
+          LLet var (LUnaryMinus (LInt 10)) (LPlus (LInt 42) (LVar v)) ->
+            var `shouldBe` v
+          expr -> do
+            print expr
+            assertFailure "Unable to match pattern"
+
+      it "handles makeAtomic with nested input" do
+        lexprE <- snailToLExpr "(- (- (- 10)))"
+        lexprE `shouldBe` Right (LUnaryMinus (LUnaryMinus (LUnaryMinus (LInt 10))))
+        let lexpr = unsafeFromRight lexprE
+        LProgram () nonComplexAst <- runLogM $ removeComplexOperands $ LProgram () lexpr
+        case nonComplexAst of
+          LLet
+            x
+            (LUnaryMinus (LInt 10))
+            ( LLet
+                y
+                (LUnaryMinus (LVar a))
+                (LUnaryMinus (LVar b))
+              ) -> do
+              x `shouldBe` a
+              y `shouldBe` b
+          expr -> do
+            print expr
+            assertFailure "Unable to match pattern"
+
+      it "handles makeAtomic with duplicate expressions" do
+        lexprE <- snailToLExpr "(+ (- 10) (- 10))"
+        lexprE `shouldBe` Right (LPlus (LUnaryMinus (LInt 10)) (LUnaryMinus (LInt 10)))
+        let lexpr = unsafeFromRight lexprE
+        LProgram () nonComplexAst <- runLogM $ removeComplexOperands $ LProgram () lexpr
+        case nonComplexAst of
+          LLet
+            x
+            (LUnaryMinus (LInt 10))
+            ( LLet
+                y
+                (LUnaryMinus (LInt 10))
+                (LPlus (LVar a) (LVar b))
+              ) -> do
+              x `shouldBe` a
+              y `shouldBe` b
+          LLet var (LUnaryMinus (LInt 10)) (LPlus (LInt 42) (LVar v)) ->
+            var `shouldBe` v
+          expr -> do
+            print expr
+            assertFailure "Unable to match pattern"
+
+      it "doesn't modify ast with non-complex operations" do
+        lexprE <- snailToLExpr "(let (a 42) (let (b a) b))"
+        lexprE `shouldBe` Right (LLet "a" (LInt 42) (LLet "b" (LVar "a") (LVar "b")))
+        let lexpr = unsafeFromRight lexprE
+        LProgram () nonComplexAst <- runLogM $ removeComplexOperands $ LProgram () lexpr
+        nonComplexAst `shouldBe` lexpr
+
 snailToLVar :: Text -> IO (Either LangError LVar)
 snailToLVar input = do
   snail <-
@@ -39,3 +196,24 @@ snailToLVar input = do
       Right _ -> assertFailure "More than one s-expression"
       _ -> assertFailure "Unable to parse snail program"
   runM (parseLVar snail)
+
+snailToLExpr :: Text -> IO (Either LangError LExpr)
+snailToLExpr input = do
+  snail <-
+    case parseSnail input of
+      Right [snail] -> pure snail
+      Right _ -> assertFailure "More than one s-expression"
+      _ -> assertFailure "Unable to parse snail program"
+  runM (parseLExpr snail)
+
+runUniquify :: LExpr -> IO LExpr
+runUniquify lexpr = do
+  let program = evalRandT (uniquifyLExpr lexpr) $ mkStdGen 2023
+  runMWith (RenameMap {renameMap = mempty}) program >>= \case
+    Right result -> pure result
+    Left _ -> assertFailure "Unable to run uniquify"
+
+unsafeFromRight :: Either a b -> b
+unsafeFromRight = \case
+  Left _ -> error "unsafeFromRight encountered Left"
+  Right x -> x
